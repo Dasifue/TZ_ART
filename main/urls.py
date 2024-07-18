c1eadd9663a823b1e5f7ec427e18c80dcdfc71cf
dasifue <dasifue@gmail.com>
2024-07-19 00:47:10 +0600

    drf yasg documentation

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,32 @@
 "Main urls"
 
+from rest_framework.authentication import BasicAuthentication
+from rest_framework import permissions
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Articles API",
+      default_version='v1',
+      description="Техническое задание",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="dasifue@gmail.com"),
+      license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    authentication_classes=(BasicAuthentication,),
+)
+
 
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.articles.urls')),
     path('api/v1/auth/', include('apps.accounts.urls')),
